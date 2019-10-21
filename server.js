d993bc2c20f6902e8b7e53ea4f1049f6cfc4e79c
Francis Albores <mrthemetribe@gmail.com>
2019-10-21 10:48:48 +0800

    Fix trailing slash for assets

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ server.all('/*', function(req, res, next) {
  });
  server.use((req, res, next) => {
   const test = /\?[^]*\//.test(req.url);
-  if (req.url.substr(-1) !== '/' && req.url.length > 1 && !test)
+  if (req.url.substr(-1) !== '/' && req.url.length > 1 && !test && !req.url.startsWith('/static/'))
     res.redirect(301, req.url + '/');
   else
     next();
